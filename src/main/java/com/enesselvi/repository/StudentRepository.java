4a73bf47c7ad086a35fcd7cb42dfe13b6db6c06c
Muhammedeness <muhammedenes7725@gmail.com>
2025-04-04 12:17:16 +0300

    Repository de kullanılmayan kütüphaneler kaldırıldı

--- src/main/java/com/enesselvi/repository/StudentRepository.java
+++ src/main/java/com/enesselvi/repository/StudentRepository.java
@@ -1,14 +1,11 @@
 package com.enesselvi.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+
 import org.springframework.stereotype.Repository;
 
 import com.enesselvi.entites.Student;
 
-import jakarta.transaction.Transactional;
 
 
 @Repository
